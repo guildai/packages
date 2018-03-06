94e27f3de37e472a63c3ee5bca5e921f54454df4
Garrett Smith <g@rre.tt>
2018-03-06 12:08:49 -0600

    Handle keyboard interrupt for commons download

--- wikipedia/datasets/commonsdownloader_wrapper.py
+++ wikipedia/datasets/commonsdownloader_wrapper.py
@@ -1,4 +1,11 @@
+import os
+import sys
+
 from commonsdownloader import commonsdownloader
 
 if __name__ == "__main__":
-    commonsdownloader.main()
+    try:
+        commonsdownloader.main()
+    except KeyboardInterrupt:
+        sys.stderr.write("Terminating run\n")
+    sys.stderr.write("Images are available in %s\n" % os.path.abspath("."))
