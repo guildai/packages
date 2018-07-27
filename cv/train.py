5a4549093b8209f293377987675a6daf6060cc67
Garrett Smith <g@rre.tt>
2018-07-27 09:14:58 -0500

    CV wip

--- cv/train.py
+++ cv/train.py
@@ -8,7 +8,10 @@ matplotlib.use('Agg')
 
 import tensorflow as tf
 
-from object_detection import model_main
+try:
+    from object_detection import model_main
+except ImportError as e:
+    sys.stderr.write("ERROR: %s\n" % e)
 
 def main():
     args = _parse_args()
@@ -17,13 +20,17 @@ def main():
 
 def _parse_args():
     p = argparse.ArgumentParser()
-    p.add_argument("--model-dir")
+    p.add_argument(
+        "--model-dir", metavar="DIR",
+        help="directory to write model checkpoints and logs")
+    p.add_argument(
+        "config", metavar="CONFIG",
+        help="path to detection config")
     return p.parse_args()
 
 def _model_main_argv(args):
     argv = [sys.argv[0]]
-    # TODO: either generate config or get from args
-    argv.extend(["--pipeline_config_path", "pet-detector.config"])
+    argv.extend(["--pipeline_config_path", args.config])
     if args.model_dir:
         argv.extend(["--model_dir", args.model_dir])
     return argv
