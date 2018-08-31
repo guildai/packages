a0cd6e4dd5870cfc6330ff90cb8390b44631aacd
Garrett Smith <g@rre.tt>
2018-08-31 17:10:49 -0500

    Support for prepared datasets for slim train

--- cv/slim/slim_train_prepared.py
+++ cv/slim/slim_train_prepared.py
@@ -16,15 +16,22 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
+import collections
+import glob
+import re
+import os
 import sys
 
 import tensorflow as tf
 
 sys.path.insert(0, "slim")
 
+from datasets import dataset_factory
+from datasets import dataset_utils
+
 import train_image_classifier
 
-from datasets import dataset_factory
+custom = collections.namedtuple("custom", "get_split")
 
 def main(argv):
     if "--dataset_name" in argv:
@@ -32,56 +39,58 @@ def main(argv):
     if "--model_name" not in argv:
         _error("--model_name is required")
     dataset_factory.datasets_map = {
-        "custom": globals()
+        "custom": custom(get_split=get_split)
     }
     argv = argv + ["--dataset_name", "custom"]
     tf.app.run(train_image_classifier.main, argv)
 
-def get_split(split_name, dataset_dir):
-    reader = reader or tf.TFRecordReader
-
-    if split_name not in SPLITS_TO_SIZES:
-        raise ValueError('split name %s was not recognized.' % split_name)
-
-    if not file_pattern:
-        file_pattern = _FILE_PATTERN
-    file_pattern = os.path.join(dataset_dir, file_pattern % split_name)
-
-
-    if reader is None:
-        reader = tf.TFRecordReader
-
-    keys_to_features = {
-        'image/encoded': tf.FixedLenFeature((), tf.string, default_value=''),
-        'image/format': tf.FixedLenFeature((), tf.string, default_value='png'),
-        'image/class/label': tf.FixedLenFeature(
-            [], tf.int64, default_value=tf.zeros([], dtype=tf.int64)),
-    }
-
-    items_to_handlers = {
-        'image': slim.tfexample_decoder.Image(),
-        'label': slim.tfexample_decoder.Tensor('image/class/label'),
-    }
-
-    decoder = slim.tfexample_decoder.TFExampleDecoder(
-        keys_to_features, items_to_handlers)
-
-    labels_to_names = None
-    if dataset_utils.has_labels(dataset_dir):
-        labels_to_names = dataset_utils.read_label_file(dataset_dir)
-
-    tfrecord_pattern = os.path.join(
-        dataset_dir,
-        "*{}-*-*.tfrecord" % split_name)
-
-    return slim.dataset.Dataset(
-        data_sources=tfrecord_pattern,
+def get_split(split_name, dataset_dir, file_pattern=None, reader=None):
+    assert file_pattern is None, file_pattern
+    assert reader is None, reader
+    source_pattern = os.path.join(dataset_dir, "*%s-*-*.tfrecord" % split_name)
+    example_count = _example_count_from_sources(source_pattern)
+    labels = dataset_utils.read_label_file(dataset_dir)
+    return tf.contrib.slim.dataset.Dataset(
+        data_sources=source_pattern,
         reader=tf.TFRecordReader,
-        decoder=decoder,
-        num_samples=,
-        items_to_descriptions=_ITEMS_TO_DESCRIPTIONS,
-        num_classes=_NUM_CLASSES,
-        labels_to_names=labels_to_names)
+        decoder=_decoder(),
+        num_samples=example_count,
+        items_to_descriptions=_item_descriptions(),
+        num_classes=len(labels),
+        labels_to_names=labels)
+
+def _example_count_from_sources(source_pattern):
+    sources = glob.glob(source_pattern)
+    if not sources:
+        _error("no files matching '%s'" % source_pattern)
+    count = 0
+    for source in sources:
+        m = re.search("[0-9]+-([0-9]+)", source)
+        if m:
+            count = max(count, int(m.group(1)))
+    if count == 0:
+        _error("could not get example count from files '%s'" % source_pattern)
+    return count
+
+def _decoder():
+    keys_to_features = {
+        "image/encoded": tf.FixedLenFeature((), tf.string),
+        "image/format": tf.FixedLenFeature((), tf.string),
+        "image/class/label": tf.FixedLenFeature([], tf.int64),
+    }
+    items_to_handlers = {
+        "image": tf.contrib.slim.tfexample_decoder.Image(),
+        "label": tf.contrib.slim.tfexample_decoder.Tensor("image/class/label"),
+    }
+    return tf.contrib.slim.tfexample_decoder.TFExampleDecoder(
+        keys_to_features,
+        items_to_handlers)
+
+def _item_descriptions():
+    return {
+        "image": "Image",
+        "label": "Image label",
+    }
 
 def _error(msg):
     sys.stderr.write("%s: %s\n" % (sys.argv[0], msg))
