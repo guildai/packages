46501d5d3e3c65e510a16afc63691acbc98b756c
Garrett Smith <g@rre.tt>
2018-09-19 14:56:08 -0500

    Work on filling in Slim support

--- package.mk
+++ package.mk
@@ -8,3 +8,8 @@ upload: clean
 	export TWINE_USERNAME=guildai; \
 	export TWINE_PASSWORD=`gpg --quiet --batch -d $(root)/.pypi-creds.gpg`; \
 	guild package --upload --skip-existing
+
+.PHONY: test
+
+test:
+	@test ! -e test/run && echo "No tests found" || test/run
